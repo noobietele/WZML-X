c87b5bf15e5b2ce48305757c153b14ccdb53fd67
Noobie <155804401+noobietele@users.noreply.github.com>
2025-12-15 11:40:44 +0530

    Update handlers.py
    Signed-off-by: Noobie <155804401+noobietele@users.noreply.github.com>

--- bot/core/handlers.py
+++ bot/core/handlers.py
@@ -49,13 +49,6 @@ def add_handlers():
         )
     )
     TgClient.bot.add_handler(
-        MessageHandler(
-            broadcast,
-            filters=command(BotCommands.BroadcastCommand, case_sensitive=True)
-            & CustomFilters.sudo,
-        )
-    )
-    TgClient.bot.add_handler(
         CallbackQueryHandler(
             edit_bot_settings, filters=regex("^botset") & CustomFilters.sudo
         )
@@ -166,20 +159,6 @@ def add_handlers():
     )
     TgClient.bot.add_handler(
         MessageHandler(
-            jd_mirror,
-            filters=command(BotCommands.JdMirrorCommand, case_sensitive=True)
-            & CustomFilters.authorized,
-        )
-    )
-    TgClient.bot.add_handler(
-        MessageHandler(
-            nzb_mirror,
-            filters=command(BotCommands.NzbMirrorCommand, case_sensitive=True)
-            & CustomFilters.authorized,
-        )
-    )
-    TgClient.bot.add_handler(
-        MessageHandler(
             leech,
             filters=command(BotCommands.LeechCommand, case_sensitive=True)
             & CustomFilters.authorized,
@@ -189,27 +168,6 @@ def add_handlers():
         MessageHandler(
             qb_leech,
             filters=command(BotCommands.QbLeechCommand, case_sensitive=True)
-            & CustomFilters.authorized,
-        )
-    )
-    TgClient.bot.add_handler(
-        MessageHandler(
-            jd_leech,
-            filters=command(BotCommands.JdLeechCommand, case_sensitive=True)
-            & CustomFilters.authorized,
-        )
-    )
-    TgClient.bot.add_handler(
-        MessageHandler(
-            nzb_leech,
-            filters=command(BotCommands.NzbLeechCommand, case_sensitive=True)
-            & CustomFilters.authorized,
-        )
-    )
-    TgClient.bot.add_handler(
-        MessageHandler(
-            uphoster,
-            filters=command(BotCommands.UpHosterCommand, case_sensitive=True)
             & CustomFilters.authorized,
         )
     )
@@ -270,17 +228,7 @@ def add_handlers():
             filters=command(BotCommands.RestartSessionsCommand, case_sensitive=True)
             & CustomFilters.sudo,
         )
-    )
-    TgClient.bot.add_handler(
-        MessageHandler(
-            imdb_search,
-            filters=command(BotCommands.IMDBCommand, case_sensitive=True)
-            & CustomFilters.authorized,
-        )
-    )
-    TgClient.bot.add_handler(
-        CallbackQueryHandler(imdb_callback, filters=regex("^imdb"))
-    )
+    )    
     TgClient.bot.add_handler(
         MessageHandler(
             ping,
@@ -295,14 +243,6 @@ def add_handlers():
             & CustomFilters.authorized,
         )
     )
-    TgClient.bot.add_handler(
-        MessageHandler(
-            mediainfo,
-            filters=command(BotCommands.MediaInfoCommand, case_sensitive=True)
-            & CustomFilters.authorized,
-        )
-    )
-
     TgClient.bot.add_handler(
         MessageHandler(
             bot_stats,
@@ -349,27 +289,6 @@ def add_handlers():
     )
     TgClient.bot.add_handler(
         CallbackQueryHandler(edit_user_settings, filters=regex("^userset"))
-    )
-    TgClient.bot.add_handler(
-        MessageHandler(
-            ytdl,
-            filters=command(BotCommands.YtdlCommand, case_sensitive=True)
-            & CustomFilters.authorized,
-        )
-    )
-    TgClient.bot.add_handler(
-        MessageHandler(
-            ytdl_leech,
-            filters=command(BotCommands.YtdlLeechCommand, case_sensitive=True)
-            & CustomFilters.authorized,
-        )
-    )
-    TgClient.bot.add_handler(
-        MessageHandler(
-            hydra_search,
-            filters=command(BotCommands.NzbSearchCommand, case_sensitive=True)
-            & CustomFilters.authorized,
-        )
     )
     if Config.SET_COMMANDS:
         global BOT_COMMANDS
